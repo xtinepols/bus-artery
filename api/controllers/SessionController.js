2f40574e958307438fb55543c97d65122cbb821f
jMelgar101 <mjuliusvincent@gmail.com>
2017-11-30 18:53:43 +0800

    Add Session api

---
+++ api/controllers/SessionController.js
@@ -0,0 +1,133 @@
+/**
+ * SessionController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+import moment from "moment";
+
+const ownerLogin = async (req) =>
+	new Promise(async (resolve, reject) => {
+		try {
+			let { username, password } = req.body;
+
+			username = username.trim();
+			password = password.trim();
+
+			const owner = await Owner.findOne({ username });
+
+			if (!owner) {
+				throw new Error("Owner account not found.");
+			}
+
+			const isMatch = await Session.verifyPassword(password, owner.password);
+
+			if (!isMatch) {
+				throw new Error("Invalid password.");
+			}
+
+			const sessionPayload = await generateSession(req, owner.id, "ownerId");
+
+			resolve(sessionPayload);
+		} catch (err) {
+			sails.log.error("ownerLogin::error");
+			sails.log.error(err);
+
+			return reject(err);
+		}
+	});
+
+const driverLogin = async (req) =>
+	new Promise(async (resolve, reject) => {
+		try {
+			let { username, password } = req.body;
+
+			username = username.trim();
+			password = password.trim();
+
+			const driver = await Driver.findOne({ username });
+
+			if (!driver) {
+				throw new Error("Driver account not found.");
+			}
+
+			const isMatch = await Session.verifyPassword(password, driver.password);
+			
+			if (!isMatch) {
+				throw new Error("Invalid password.");
+			}
+
+			const sessionPayload = await generateSession(req, driver.id, "driverId");
+
+			resolve(sessionPayload);
+		} catch (err) {
+			sails.log.error("driverLogin::error");
+			sails.log.error(err);
+
+			return reject(err);
+		}
+	});
+
+const generateSession = async (req, id, key) => {
+	try {
+		const metadata = req.headers;
+		let session = await Session
+			.find({
+				[key]: id,
+				expiresAt: { ">": +moment() },
+			})
+			.limit(1);
+
+		session = session[0];
+
+		if (!session) {
+			session = await Session
+				.create({
+					[key]: id,
+					metadata,
+				})
+				.meta({ fetch: true });
+		}
+
+		session[key] =
+			(key === "ownerId") ? await Owner.findOne({ id }) : await Driver.findOne({ id });
+
+		return session;
+	} catch (err) {
+		sails.log.error("generateSession::error");
+		sails.log.error(err);
+
+		return err;
+	}
+};
+
+const SessionController = {
+	create: async (req, res) => {
+		try {
+			let session = {};
+
+			switch (req.body.type) {
+			case "owner":
+				session = await ownerLogin(req);
+				break;
+			case "driver":
+				session = await driverLogin(req);
+				break;
+			default:
+				throw new Error("Unknown identifier. Owner and Driver only.");
+			}
+
+			sails.log.debug("SessionController::create::done");
+			sails.log.debug(session);
+
+			res.ok(session);
+		} catch (err) {
+			sails.log.error("SessionController::create::error");
+			sails.log.error(err);
+
+			return res.badRequest(err);
+		}
+	},
+};
+
+export default SessionController;
---
+++ api/models/Session.js
@@ -0,0 +1,75 @@
+/**
+ * Session.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+import moment from "moment";
+import sha256 from "crypto-js/sha256";
+import bcrypt from "bcryptjs";
+
+const generateToken = () => {
+	return `${sha256(`${Math.floor(Math.random() * 1000)  }${  new Date().getTime()}`)  }`;
+	// return `${sha256(Math.floor(Math.random() * 1000) + new Date().getTime())}`;
+};
+
+const Session = {
+	attributes: {
+		ownerId: {
+			model: "owner",
+			description: "login either via owner or driver",
+		},
+
+		driverId: {
+			model: "driver",
+			description: "login either via owner or driver",
+		},
+
+		refreshToken: {
+			type: "string",
+		},
+
+		accessToken: {
+			type: "string",
+		},
+
+		expiresAt: {
+			type: "number",
+		},
+
+		metadata: {
+			type: "json",
+		},
+	},
+
+	beforeCreate: (values, cb) => {
+		values.refreshToken = generateToken();
+		values.accessToken = generateToken();
+		values.expiresAt = +moment().add(5, "months");
+
+		sails.log.debug("Session::beforeCreate");
+		sails.log.debug(values);
+
+		cb();
+	},
+
+	verifyPassword: async (candidatePassword, hash) => {
+		const bool = await new Promise(async (resolve, reject) => {
+			try {
+				const isMatch = await bcrypt.compare(candidatePassword, hash);
+
+				return resolve(isMatch);
+			} catch (e) {
+				sails.log.debug("LoginSession::verifyPassword::error");
+				sails.log.error(e);
+
+				return reject(e);
+			}
+		});
+
+		return bool;
+	},
+};
+
+export default Session;
